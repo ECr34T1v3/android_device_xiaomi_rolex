109653fc018de4062d863418e00c06bc8cf7a919
Ankan005 <Ankan005@gmail.com>
2019-09-18 10:44:43 +0530

    arrow bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cos_rolex.mk
+    $(LOCAL_DIR)/arrow_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    cos_rolex-userdebug
+    arrow_rolex-userdebug
